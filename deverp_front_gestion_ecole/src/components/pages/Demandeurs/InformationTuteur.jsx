cdd79af6d7635550dfb9547ef9d8236d4879df92
kara <ndiayemama868@gmail.com>
2025-02-04 18:20:40 +0000

    ajout des interfaces InformationTuteur et Decision

---
+++ deverp_front_gestion_ecole/src/components/pages/Demandeurs/InformationTuteur.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Layout from '../../formulaire/Layout'; // Assure-toi d'importer correctement le Layout
+import StudentForm from '../../formulaire/StudentForm'; // Assure-toi d'importer le formulaire que tu veux afficher
+import StepIndicator from '../../formulaire/StepIndicator';
+import TuteurForm from '../../formulaire/TuteurForm';
+
+const InformationStudentPage = () => {
+  return (
+    <Layout
+      leftText="Veuillez compléter les informations concernant le tuteur en suivant les instructions ci-dessous." 
+      formComponent={<TuteurForm/>}
+      StepIndicator={<StepIndicator activeStep={2} totalSteps={4} activeStepColor="bg-green-600" />}
+    />
+  );
+}
+
+export default InformationStudentPage;
