552df2cdee42ebf4efa7683b6e2030b3d598af9d
kara <ndiayemama868@gmail.com>
2025-02-05 23:17:03 +0000

    resolution des conflits

--- deverp_front_gestion_ecole/src/App.js
+++ deverp_front_gestion_ecole/src/App.js
@@ -1,6 +1,9 @@
 // App.js
+
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 import { FormProvider } from './context/FormContext';
+import './App.css';
+
 import InformationStudent from './components/pages/Demandeurs/InformationStudent';
 import InformationTuteur from './components/pages/Demandeurs/InformationTuteur';
 import DocAFournir from './components/pages/Demandeurs/DocAFournir';
