80c159c14bff5d21228ee8ed059f029de22fd6d5
Yves-Yangasso <yangassojeanyves@gmail.com>
2025-02-05 14:08:48 +0000

    Mise a jour front

--- deverp_front_gestion_ecole/src/App.js
+++ deverp_front_gestion_ecole/src/App.js
@@ -1,6 +1,11 @@
+<<<<<<< HEAD
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './components/pages/Admins/Decision.jsx'; // Il faut utiliser ici AppRoutes
 import './App.css';
+=======
+import logo from "./logo.svg";
+import "./App.css";
+>>>>>>> ae13001 (Mise a jour front)
 
 function App() {
   return (
