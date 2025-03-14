4014e3b2862b8205a0f9a3eb3487fb5a512373c2
Yves-Yangasso <yangassojeanyves@gmail.com>
2025-03-14 18:36:03 +0000

    envoie fac paiement

---
+++ deverp_back_gestion_ecole/resources/views/emails/test.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Email Test</title>
+</head>
+<body>
+    <h1>Bonjour,</h1>
+    <p>Ceci est un test d'envoi d'email avec Laravel et Gmail.</p>
+</body>
+</html>
